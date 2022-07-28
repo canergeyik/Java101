8b18f7bac4fe329197d6d005151f2f750cb35639
Caner Geyik <97602074+canergeyik@users.noreply.github.com>
2022-07-28 18:26:21 +0300

    Update HarmonikSeri.java

--- HarmonikSeri.java
+++ HarmonikSeri.java
@@ -5,7 +5,7 @@ public class HarmonikSeri {
         Scanner inp = new Scanner(System.in);
         System.out.println("N sayısını giriniz");
         int sayi=inp.nextInt();
-        int toplam=1,i;
+        float toplam=1,i;
         for (i=2;i<sayi+1;i++){
             toplam=toplam+(1/i);
         }
